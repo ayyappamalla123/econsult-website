3202c769fa2a86373b69b59a653394206b83724f
Debosmit Majumder <debmaj2programmania@rediffmail.com>
2020-04-09 15:54:39 +0530

    Updates

---
+++ precache-manifest.1381b0138dac608fe54663634e0bb685.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3b6804aface3f492430d07e0f60053d3",
+    "url": "/econsult-website/index.html"
+  },
+  {
+    "revision": "abd2de1435eef3122be9",
+    "url": "/econsult-website/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "79db7d4981814e0cb005",
+    "url": "/econsult-website/static/js/2.455f1b76.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/econsult-website/static/js/2.455f1b76.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "abd2de1435eef3122be9",
+    "url": "/econsult-website/static/js/main.13ff28dd.chunk.js"
+  },
+  {
+    "revision": "d101897a7dfca8f87a21",
+    "url": "/econsult-website/static/js/runtime-main.c28a2cd7.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/econsult-website/static/media/logo.5d5d9eef.svg"
+  }
+]);
